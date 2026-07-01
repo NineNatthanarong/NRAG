0496eef643886e74fa70744638ec326f72dff0e2
NineNatthanarong <106664726+NineNatthanarong@users.noreply.github.com>
2026-07-01 14:52:48 +0700

    Bump version to 0.1.1

--- nrag/__init__.py
+++ nrag/__init__.py
@@ -26,7 +26,7 @@ from ._types import (
 from .config import Config
 from .results import AddReport, Answer, Citation, CostEstimate, CostGuardError, QueryResult
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = [
     "Nrag",
